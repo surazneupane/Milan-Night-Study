1282a8046b1f1cb51bcb13da060b1fb2d2839b0e
Suraj <suraznep@gmail.com>
2021-04-03 14:10:30 +0545

    Function Basic Study

--- index.php
+++ index.php
@@ -1,10 +1,74 @@
 <?php 
+// Switch , function , function loop ,  return type
+$whatTODo = readline('Enter What To Do:::');
 
-// require_once ".Cli.php";
-// require_once "./star.php";
-// require_once "./fibo.php";
-// require_once "./oddeven.php";
+switch($whatTODo)
+    {
+        case 1:
+            userAgeVerification();
+            break;
+        case 2:
+            printStar();
+            break;
+        case 3:
+            fibo();
+            break;
+         case 4:
+            oddEven();
+            break;
+          case 5:
+            oddEven();
+            break;
+
+        case 6:
+          echo printMyName();
+          
+            break;
+        
+        default:
+        echo "Please Enter Valid Input\n";
+
+
+    }
+
+
+function userAgeVerification()
+{
+    require_once "./Cli.php";
+
+}
+
+function printStar()
+{
+require_once "./star.php";
+
+}
+
+function fibo()
+{
+require_once "./fibo.php";
+
+}
+
+function oddEven()
+{
+require_once "./oddeven.php";
+
+}
+
+function primeNumber()
+{
+
 require_once "./prime.php";
+}
+
+
+
+function printMyName() : string
+{
+   return readline("Enter Your Name::");
+  
+}
 
 
 ?>
